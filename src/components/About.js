2896d793397002f2b6ab75c7b96d8f9bba5dd9df
FaithKamau <faith.kamau@student.moringaschool.com>
2023-06-09 22:35:53 +0300

    Completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I am learning how to code React</p>
+      <img src="https://i.imgur.com/mV8PQxj.gif"
+ alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
